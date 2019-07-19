9ac0f8c07265c6a674d5aa5d4b75f89c3cb9fb30
gray <shinjoohyung25@gmail.com>
2019-07-19 14:29:40 -0400

    fix on optimizer

--- mnist.py
+++ mnist.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+import torch.nn.functional as F
 
 train_dataset = torchvision.datasets.MNIST(root='./data',train=True, transform=transforms.ToTensor(), download=True)
 test_dataset = torchvision.datasets.MNIST(root='./data',train=False, transform=transforms.ToTensor(), download = True)
@@ -17,19 +18,30 @@ class Net(nn.Module):
 
     def forward(self, x):
         output = self.layer1(x)
+        output = F.sigmoid(output)
         output = self.layer2(output)
         return output
 
-lossFunction = nn.CrossEntropyLoss()
+
 
 net  = Net()
+
+lossFunction = nn.CrossEntropyLoss()
+learning_rate = 0.001
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+
 num_epochs = 5
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images,labels) in enumerate(train_loader):
         images = images.reshape(-1,28*28)
         out = net(images)
+        
         loss = lossFunction(out, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
 
 
 correct = 0
